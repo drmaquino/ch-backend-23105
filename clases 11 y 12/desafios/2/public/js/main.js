e2ea46f1ab5783587218100295985fc5ff142831
mariano aquino <mariano.aquino@gmail.com>
2021-09-16 14:37:13 -0300

    agrega clase 11 y 12

---
+++ clases 11 y 12/desafios/2/public/js/main.js
@@ -0,0 +1,10 @@
+const socket = io();
+
+const input = document.querySelector('input')
+input.addEventListener('input', () => {
+    socket.emit('mensaje', input.value);
+});
+
+socket.on('mensajes', data => {
+    document.querySelector('p').innerText = data
+});
